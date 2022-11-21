736c7e17640b4af53867ba039853d4ebceded8ad
Melanie Deuretzbacher <melanie.deuretzbacher@ingka.ikea.com>
2022-11-21 20:52:26 +0100

    added form for adding new events

--- src/views/AddEvent.jsx
+++ src/views/AddEvent.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./../css/views/addevent.module.scss";
+import AddEventForm from "./../components/AddEventForm";
 
 const AddEvent = () => {
   return(
     <div className={styles.addEventWrapper}>
       <h2>Add new Event</h2>
+      <p>Let's get together!</p>
+      <AddEventForm />
     </div>
   );
 }

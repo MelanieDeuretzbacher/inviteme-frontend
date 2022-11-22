17bfcbc1b139c2e3e2d829d807e4ff0663dcd2b4
Melanie Deuretzbacher <melanie.deuretzbacher@ingka.ikea.com>
2022-11-22 09:56:50 +0100

    added highlight on navitem if active

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./../css/navbar.module.scss";
 
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState();
+
+  useEffect(() => {
+    setActiveNav("home");
+  }, [])
+
+  const toggleActive = (e, item) => {
+    e.stopPropagation();
+    setActiveNav(item);
+  }
+
   return(
     <nav className={styles.nav}>
       <ul className={styles.navList}>
-      <li className={styles.navListItem}>
+      <li className={`${activeNav === "home" ? styles.activeNavItem : ""} ${styles.navListItem}`} onClick={e => {toggleActive(e, "home")}}>
           <Link to="/">
-            <img src="assets/images/icons/home-icon.svg" />
+            <img alt="house icon" src="assets/images/icons/home-icon.svg" />
             <p>Home</p>
           </Link>
         </li>
-        <li className={styles.navListItem}>
+        <li className={`${activeNav === "add" ? styles.activeNavItem : ""} ${styles.navListItem}`} onClick={e => {toggleActive(e, "add")}}>
           <Link to="/add">
-            <img src="assets/images/icons/add-icon.svg" />
+            <img alt="plus icon" src="assets/images/icons/add-icon.svg" />
             <p>Add</p>
           </Link>
         </li>
-        <li className={styles.navListItem}>
+        <li className={`${activeNav === "pending" ? styles.activeNavItem : ""} ${styles.navListItem}`} onClick={e => {toggleActive(e, "pending")}}>
           <Link to="/pending">
-            <img src="assets/images/icons/pending-icon.svg" />
+            <img alt="clock icon" src="assets/images/icons/pending-icon.svg" />
             <p>Pending</p>
           </Link>
         </li>
